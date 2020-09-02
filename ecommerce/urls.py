d3ab7faa6e54929cc3c4b56f38623c48f61e3826
yakamurii <30423462+yakamurii@users.noreply.github.com>
2020-09-02 20:50:56 -0300

    Add files via upload

---
+++ ecommerce/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+	path('api/', include('sistema_login_cadastro.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
+]
